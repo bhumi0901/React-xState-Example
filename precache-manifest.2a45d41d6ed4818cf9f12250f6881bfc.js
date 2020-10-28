fbc6abad2e4e684ea63c1cb7beb04f6731d8ff9b
Bhumi Sukhadiya <bhumika.sukhadiya.9@gmail.com>
2020-10-28 17:45:34 +0530

    Updates

---
+++ precache-manifest.2a45d41d6ed4818cf9f12250f6881bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b171996f79da414c79e9c08c772097bd",
+    "url": "/React-xState-Example/index.html"
+  },
+  {
+    "revision": "44786f82c67c2832b966",
+    "url": "/React-xState-Example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "249b6ea92101fcfd6385",
+    "url": "/React-xState-Example/static/js/2.73d657cc.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/React-xState-Example/static/js/2.73d657cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44786f82c67c2832b966",
+    "url": "/React-xState-Example/static/js/main.4ff55503.chunk.js"
+  },
+  {
+    "revision": "673237371ebf0300fe2e",
+    "url": "/React-xState-Example/static/js/runtime-main.8f5d1c68.js"
+  }
+]);
